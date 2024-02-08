8e49fa9b1e742ea4724272f50ce9212c808530e6
arieltroian <arieltroian@outlook.com>
2024-02-08 16:42:23 -0300

    feat: add footer component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -43,6 +43,17 @@ export default async function Home() {
           ))}
         </div>
       </div>
+
+      <div className="mt-6 mb-[4.5rem]">
+        <h2 className="px-5 mb-3 text-xs uppercase text-gray-400 font-bold">
+          Populares
+        </h2>
+        <div className="flex gap-4 px-5 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {petshops.map((petshop) => (
+            <PetShopItem key={petshop.id} petshop={petshop} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
