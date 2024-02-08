ac7cf027af8b49118607f572b5097fe66751726c
arieltroian <arieltroian@outlook.com>
2024-02-08 16:31:45 -0300

    feat: add initial petshop item

---
+++ app/(home)/_components/petshop-item.tsx
@@ -0,0 +1,40 @@
+import { Button } from "@/app/_components/ui/button";
+import { Card, CardContent } from "@/app/_components/ui/card";
+import { Petshop } from "@prisma/client";
+import Image from "next/image";
+
+interface PetShopItemProps {
+  petshop: Petshop;
+}
+
+const PetShopItem = ({ petshop }: PetShopItemProps) => {
+  return (
+    <Card className="min-w-[167px] max-w-[167px] rounded-2xl">
+      <CardContent className="p-0">
+        <div className="relative w-full h-[159px]">
+          <Image
+            src={petshop.imageUrl}
+            style={{ objectFit: "cover" }}
+            alt={petshop.name}
+            fill
+            sizes="100vw"
+            className="rounded-2xl"
+          />
+        </div>
+        <div className="px-3 pb-3  ">
+          <h2 className="font-bold mt-2 overflow-hidden text-ellipsis text-nowrap">
+            {petshop.name}
+          </h2>
+          <p className="text-sm text-gray-400 overflow-hidden text-ellipsis text-nowrap">
+            {petshop.address}
+          </p>
+          <Button className="w-full mt-3" variant="secondary">
+            Reservar
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default PetShopItem;
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,8 +3,13 @@ import Header from "../_components/header";
 import { format } from "date-fns";
 import Search from "./_components/search";
 import BookingItem from "../_components/booking-item";
+import { db } from "../_lib/prisma";
+import PetShopItem from "./_components/petshop-item";
 
-export default function Home() {
+export default async function Home() {
+  // Chamar prisma e pegar pet shops
+  const petshops = await db.petshop.findMany({});
+
   return (
     <div>
       <Header />
@@ -26,6 +31,17 @@ export default function Home() {
           Agendamentos
         </h2>
         <BookingItem />
+      </div>
+
+      <div className="mt-6">
+        <h2 className="px-5 mb-3 text-xs uppercase text-gray-400 font-bold">
+          Recomendados
+        </h2>
+        <div className="flex gap-4 px-5 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {petshops.map((petshop) => (
+            <PetShopItem key={petshop.id} petshop={petshop} />
+          ))}
+        </div>
       </div>
     </div>
   );
