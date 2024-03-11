79b509cad037fce1736b015e63c76b06671a206b
arieltroian <arieltroian@outlook.com>
2024-03-11 14:23:35 -0300

    feat: add icon to the search component

--- app/(home)/_components/search.tsx
+++ app/(home)/_components/search.tsx
@@ -1,16 +1,11 @@
 "use client";
 
-import { Button } from "@/app/_components/ui/button";
 import { Input } from "@/app/_components/ui/input";
-import { SearchIcon } from "lucide-react";
 
 const Search = () => {
   return (
-    <div className="flex items-center gap-2">
-      <Input placeholder="Busque por um Pet Shop..." />
-      <Button variant="default">
-        <SearchIcon size={20} />
-      </Button>
+    <div className="w-full">
+      <Input placeholder="Busque por um Pet Shop..." type="search" />
     </div>
   );
 };
